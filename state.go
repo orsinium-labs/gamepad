77cb767e642dfd9fa042c0bd4e84722fe9f66418
gram <mail@orsinium.dev>
2021-06-14 14:13:06 +0200

    don't use pointer on State

--- state.go
+++ state.go
@@ -16,63 +16,63 @@ type State struct {
 }
 
 // A button is pressed
-func (s *State) A() bool {
+func (s State) A() bool {
 	return s.buttons&0b_0001 == 1
 }
 
 // B button is pressed
-func (s *State) B() bool {
+func (s State) B() bool {
 	return s.buttons&0b_0010 == 1
 }
 
 // X button is pressed
-func (s *State) X() bool {
+func (s State) X() bool {
 	return s.buttons&0b_0100 == 1
 }
 
 // Y button is pressed
-func (s *State) Y() bool {
+func (s State) Y() bool {
 	return s.buttons&0b_1000 == 1
 }
 
 // Left Bumper is pressed
-func (s *State) LB() bool {
+func (s State) LB() bool {
 	return s.buttons&0b_0001_0000 == 1
 }
 
 // Right Bumper is pressed
-func (s *State) RB() bool {
+func (s State) RB() bool {
 	return s.buttons&0b_0010_0000 == 1
 }
 
 // Back button is pressed
-func (s *State) Back() bool {
+func (s State) Back() bool {
 	return s.buttons&0b_0100_0000 == 1
 }
 
 // Start button is pressed
-func (s *State) Start() bool {
+func (s State) Start() bool {
 	return s.buttons&0b_1000_0000 == 1
 }
 
 // Guide button is pressed
-func (s *State) Guide() bool {
+func (s State) Guide() bool {
 	return s.buttons&0b_0001_0000_0000 == 1
 }
 
 // Left Stick Button is pressed
-func (s *State) LSB() bool {
+func (s State) LSB() bool {
 	return s.buttons&0b_0010_0000_0000 == 1
 }
 
 // Right Stick Button is pressed
-func (s *State) RSB() bool {
+func (s State) RSB() bool {
 	return s.buttons&0b_0100_0000_0000 == 1
 }
 
 // Left Stick coordinates, from -100 to 100.
 // Up position is negative.
-func (s *State) LS() image.Point {
+func (s State) LS() image.Point {
 	return image.Point{
 		X: s.axis[0] / axisMod,
 		Y: s.axis[1] / axisMod,
@@ -81,7 +81,7 @@ func (s *State) LS() image.Point {
 
 // Right Stick coordinates, from -100 to 100.
 // Up position is negative.
-func (s *State) RS() image.Point {
+func (s State) RS() image.Point {
 	return image.Point{
 		X: s.axis[3] / axisMod,
 		Y: s.axis[4] / axisMod,
@@ -89,31 +89,31 @@ func (s *State) RS() image.Point {
 }
 
 // Directional Pad left button is pressed
-func (s *State) DPadLeft() bool {
+func (s State) DPadLeft() bool {
 	return s.axis[6] < 0
 }
 
 // Directional Pad right button is pressed
-func (s *State) DPadRight() bool {
+func (s State) DPadRight() bool {
 	return s.axis[6] > 0
 }
 
 // Directional Pad up button is pressed
-func (s *State) DPadUp() bool {
+func (s State) DPadUp() bool {
 	return s.axis[7] < 0
 }
 
 // Directional Pad down button is pressed
-func (s *State) DPadDown() bool {
+func (s State) DPadDown() bool {
 	return s.axis[7] > 0
 }
 
 // Left Trigger state from -100 to 100 where 100 is pressed.
-func (s *State) LT() int {
+func (s State) LT() int {
 	return s.axis[2] / axisMod
 }
 
 // Right Trigger state from -100 to 100 where 100 is pressed.
-func (s *State) RT() int {
+func (s State) RT() int {
 	return s.axis[5] / axisMod
 }
